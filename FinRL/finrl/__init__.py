da71d8a4710557292627fc551d4328962e9b0e2a
Graham-Broughton <broughton.graham@gmail.com>
2022-12-18 14:42:36 -0800

    removed excess libraries

--- FinRL/finrl/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from finrl.test import test
-from finrl.trade import trade
-from finrl.train import train
--- FinRL/finrl/__main__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from finrl.main import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
